6cd03593e25d9dfd9dc1edfb6c4dccf57ae70e29
Stefan Safranek <stefan@stefan>
2020-12-08 17:52:53 -0800

    cache control wrapper

--- server.go
+++ server.go
@@ -3,12 +3,18 @@ package lemur
 import (
 	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/schollz/httpfileserver"
 	"github.com/sjsafranek/lemur/middleware"
 	"github.com/sjsafranek/logger"
+)
+
+var (
+	cacheSince = time.Now().Format(http.TimeFormat)
+	cacheUntil = time.Now().AddDate(0, 0, 1).Format(http.TimeFormat)
 )
 
 func Vars(r *http.Request) map[string]string {
@@ -28,8 +34,18 @@ type HttpRouter struct {
 	*mux.Router
 }
 
+func CacheControlWrapper(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "max-age=86400, must-revalidate, public") // 30 days
+		w.Header().Set("Last-Modified", cacheSince)
+		w.Header().Set("Expires", cacheUntil)
+		h.ServeHTTP(w, r)
+	})
+}
+
 func (self *HttpRouter) AttachFileServer(route, directory string) {
-	self.PathPrefix("/static/").Handler(httpfileserver.New("/static/", directory))
+	fs := CacheControlWrapper(httpfileserver.New("/static/", directory))
+	self.PathPrefix("/static/").Handler(fs)
 }
 
 func (self *HttpRouter) AttachHandlerFunc(pattern string, handler http.HandlerFunc, methods []string) {
